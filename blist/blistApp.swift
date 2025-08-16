46fb02d419fa6228e46d9dccec32928989de863a
brian <3604068+brian-greeson@users.noreply.github.com>
2025-08-16 12:51:40 -0600

    Initial Commit

---
+++ blist/blistApp.swift
@@ -0,0 +1,17 @@
+//
+//  blistApp.swift
+//  blist
+//
+//  Created by Brian Greeson on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct blistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ blistTests/blistTests.swift
@@ -0,0 +1,17 @@
+//
+//  blistTests.swift
+//  blistTests
+//
+//  Created by Brian Greeson on 8/16/25.
+//
+
+import Testing
+@testable import blist
+
+struct blistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
